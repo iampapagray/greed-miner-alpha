5cdbcc9fb5bb5a250df704a9fc140f30c5e18420
John Graham <iampapagray@gmail.com>
2024-08-10 23:03:22 +0000

    testing layout"

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <div className="flex flex-col h-screen pb-4 bg-slate-800 overflow-hidden">
       <div className="flex flex-col p-2 h-screen w-full border-green-500 border shadow-xl">
         <div className="border-2 border-red-500 h-full flex justify-center items-center">
-          <h1 className="text-white  text-4xl">Playing with Borders</h1>
+          <h1 className="text-white  text-4xl">Testing Layout</h1>
         </div>
       </div>
       <div className="flex w-full h-[63px] bg-blue-800"></div>
