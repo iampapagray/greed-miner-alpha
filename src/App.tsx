8b0e97ffb5887da8752dc4ded3853373dae0e684
John Graham <iampapagray@gmail.com>
2024-08-10 22:47:06 +0000

    testing borders

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,15 @@ import "@twa-dev/sdk";
 function App() {
   const { network } = useTonConnect();
 
-  return <div className="flex h-screen bg-slate-800"></div>;
+  return (
+    <div className="flex h-screen bg-slate-800 overflow-hidden">
+      <div className="flex flex-col p-2 h-screen w-full border-green-500 border shadow-xl">
+        <div className="border-2 border-red-500 h-full flex justify-center items-center">
+          <h1 className="text-white  text-4xl">Playing with Borders</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
