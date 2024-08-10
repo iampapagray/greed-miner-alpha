14405cb7091c02e2d124fd04198bbbdea0175b9c
John Graham <iampapagray@gmail.com>
2024-08-10 22:56:04 +0000

    testing layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,13 @@ function App() {
   const { network } = useTonConnect();
 
   return (
-    <div className="flex h-screen bg-slate-800 overflow-hidden">
+    <div className="flex flex-col h-screen pb-4 bg-slate-800 overflow-hidden">
       <div className="flex flex-col p-2 h-screen w-full border-green-500 border shadow-xl">
         <div className="border-2 border-red-500 h-full flex justify-center items-center">
           <h1 className="text-white  text-4xl">Playing with Borders</h1>
         </div>
       </div>
+      <div className="flex w-full h-[63px] bg-blue-800"></div>
     </div>
   );
 }
