9d0ed2b104c55c8c7566f5abe7b881882b20f165
John Graham <iampapagray@gmail.com>
2024-08-11 01:25:21 +0000

    Started Mine

--- src/views/Mine.tsx
+++ src/views/Mine.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MinePage() {
   return (
-    <div className="h-full flex justify-center items-center">
+    <div className="border border-red-500 h-full flex justify-center items-center">
       <h1 className="text-white  text-4xl">Let's Mine ⛏️</h1>
     </div>
   );
