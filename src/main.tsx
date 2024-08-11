5f7f54809b6f2913caff7cd5777d359d41344864
John Graham <iampapagray@gmail.com>
2024-08-11 01:28:40 +0000

    removed slash

--- src/main.tsx
+++ src/main.tsx
@@ -24,7 +24,7 @@ const queryClient = new QueryClient({
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
+    <Route element={<App />}>
       <Route index element={<MinePage />} />
       <Route path="refinery" element={<RefineryPage />} />
       <Route path="market" element={<MarketPage />} />
