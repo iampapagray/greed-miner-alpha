85938788e9c51d0d097fda39ec1c82ef6792dfc7
John Graham <iampapagray@gmail.com>
2024-08-11 01:38:26 +0000

    testing router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="flex flex-col h-screen bg-slate-800 overflow-hidden">
+    <div className="flex flex-col h-screen bg-[#18181B] overflow-hidden">
       <div className="flex flex-col px-[14px] flex-1 w-full  shadow-xl">
         <Outlet />
       </div>
-      <NavigationBar />
+      {/* <NavigationBar /> */}
     </div>
   );
 }
