81c2029d60e14498093c9371f85e6bbf8d06a6f4
John Graham <iampapagray@gmail.com>
2024-08-12 23:40:08 +0000

    fix 2

--- src/views/Mine.tsx
+++ src/views/Mine.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function MinePage() {
   return (
     <div className="border border-red-500 h-full flex justify-center items-center">
-      <h1 className="text-white  text-4xl">Let's Mine o ⛏️</h1>
+      <h1 className="text-white  text-4xl">Let's Mine Up ⛏️</h1>
     </div>
   );
 }
