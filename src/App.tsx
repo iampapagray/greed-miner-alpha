2ec0e35d63068f2f6d12ce6c5a7cce7511503fb7
John Graham <iampapagray@gmail.com>
2024-08-17 21:45:10 +0000

    nav bug try 3

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="flex flex-col h-screen bg-gradient-to-b from-[#1D1001] via-[#936C06] to-[#936C06] bg-[length:100%_400%] bg-[position:0%_0%] overflow-hiddens">
+    <div className="flex flex-col h-screen bg-gradient-to-b from-[#1D1001] via-[#936C06] to-[#936C06] bg-[length:100%_400%] bg-[position:0%_0%] overflow-hidden">
       <div className="flex flex-col px-[14px] flex-1 w-full  shadow-xl">
         <Outlet />
       </div>
