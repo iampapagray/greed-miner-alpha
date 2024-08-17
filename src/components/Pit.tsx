e6841e4b266c07083a676914505d25612afb3ec7
John Graham <iampapagray@gmail.com>
2024-08-17 18:08:28 +0000

    fake buttons

--- src/components/Pit.tsx
+++ src/components/Pit.tsx
@@ -8,7 +8,7 @@ function Pit() {
     <div className='relative h-full w-full'>
         <div className="absolute bottom-0 top-0 -left-3 -right-3 bg-[url('/src/assets/images/mineBg.svg')] bg-center bg-no-repeat bg-contain px-[2.65rem] py-[6.4rem]">
           {/* <img src={MineBackground} className="absolute h-full w-full z-1" /> */}
-          <div className="w-full h-full grid grid-cols-5 grid-row-5">
+          <div className="w-full h-full grid grid-cols-5 grid-row-5 gap-1">
             {buttonNumbers.map((number, i) => (
               <button key={i} className="bg-amber-950 border border-amber-900 rounded-lg flex justify-center items-center">
                 {number}
